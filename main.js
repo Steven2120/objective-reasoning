4a4678206c0db095dd40d49815373b8bb64c1313
Steven Moyano <stevenmoyano1@gmail.com>
2021-03-10 20:09:20 -0600

    completed required tests

--- main.js
+++ main.js
@@ -18,13 +18,27 @@ function getPlaylistLength(list) {
 }
 
 function getHardestHomework(hardest) {
-    hardest.reduce(function (sol, obj) {
-        if (obj.averageScore < sol.averageScore) {
-            return obj.name;
-        } else {
-            return sol.name;
-        } 
-    })
+    //define variables to act as placeholders for lowest scores and the name of that hw with the lowest score
+    let lowestScore = 999999999;
+    let nameOfHardestHw = "";
+    //loops through array of objects and if score is lower than lowestScore it will store score each iteration of loop in lowestScore variable and will also push the name of it to nameOfHardestHw.
+    for (const item of hardest) {
+        if (item.averageScore < lowestScore) {
+            lowestScore = item.averageScore;
+            nameOfHardestHw = item.name;
+        }
+    }
+    //returns the name of the hardest hw
+    return nameOfHardestHw;
+}
+
+function createPhonebook(name, number) {
+    //defines variable to an empty object that will store solution
+    let final = {};
+    //loops through both name and number arrays with forEach method and inserts name as keys and number as values
+    name.forEach((name, i) => final[name] = number[i])
+    //returns object
+    return final;
 }
 
 
